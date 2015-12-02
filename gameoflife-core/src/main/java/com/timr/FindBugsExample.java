3cc6712e265c686dcb708e9a9d5e2a4ca6305946
Tim Reynolds <timr_317@yahoo.com>
2015-12-01 20:43:21 -0800

    add test to force USBR_UNNECESSARY_STORE_BEFORE_RETURN

---
+++ gameoflife-core/src/main/java/com/timr/FindBugsExample.java
@@ -0,0 +1,10 @@
+package com.timr;
+/*
+ * USBR_UNNECESSARY_STORE_BEFORE_REETURN
+ */
+public class FindBugsExample {
+	public String FBExample() {
+		String answer = "This is the answer";
+		return answer;
+	}
+}
