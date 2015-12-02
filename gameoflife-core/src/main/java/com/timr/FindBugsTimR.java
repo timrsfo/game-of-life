c2178f65d17bc8f63daab1305480e8df7008d68b
Tim Reynolds <timr_317@yahoo.com>
2015-12-01 23:12:07 -0800

    conflict with FindBugsExample

--- gameoflife-core/src/main/java/com/timr/FindBugsTimR.java
+++ gameoflife-core/src/main/java/com/timr/FindBugsTimR.java
@@ -2,7 +2,7 @@ package com.timr;
 /*
  * USBR_UNNECESSARY_STORE_BEFORE_REETURN
  */
-public class FindBugsExample {
+public class FindBugsExampleTimR {
 	public String FBExample() {
 		String answer = "This is the answer";
 		return answer;
