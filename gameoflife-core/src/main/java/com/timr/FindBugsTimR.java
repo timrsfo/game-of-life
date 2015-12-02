88d72578a55e81adac12088decedaac39086b7c8
Tim Reynolds <timr_317@yahoo.com>
2015-12-01 23:15:13 -0800

    FindBugsTimR

--- gameoflife-core/src/main/java/com/timr/FindBugsTimR.java
+++ gameoflife-core/src/main/java/com/timr/FindBugsTimR.java
@@ -2,7 +2,7 @@ package com.timr;
 /*
  * USBR_UNNECESSARY_STORE_BEFORE_REETURN
  */
-public class FindBugsExampleTimR {
+public class FindBugsTimR {
 	public String FBExample() {
 		String answer = "This is the answer";
 		return answer;
